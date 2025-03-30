e017ac597b6e981815faf8639ecf8189569c5484
Ramil-hp <bakalawra@gmail.com>
2025-03-31 00:17:16 +0500

    Update views.py

--- common/views.py
+++ common/views.py
@@ -2,7 +2,7 @@ class TitleMixin:
     title = None
 
     def get_context_data(self,**kwargs):
-        # Для того чтобы работал основной гет
+        # In order for the basic geth to work
         context = super(TitleMixin,self).get_context_data(**kwargs)
         context['title'] = self.title
         return context
